83180a5f30c6576b43389cb2f195f92fd1a50956
Iroberto <Ivanrobertomaneroii7@gmail.com>
2019-02-06 10:43:00 +0100

    Fin de la seccion 6

---
+++ src/app/services/info-pagina.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { InfoPagina } from '../interfaces/info-pagina.interfaces';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InfoPaginaService {
+
+  info: any = {};
+  equipo: InfoPagina[] = [];
+
+  cargada = false;
+
+  constructor( private http: HttpClient  ) {
+
+    this.cargarInfo();
+    this.cargarEquipo();
+
+   }
+
+   private cargarInfo() {
+
+    this.http.get('assets/data/data-pagina.json').subscribe((resp: InfoPagina) => {
+
+      this.cargada = true;
+      this.info = resp;
+
+       } );
+
+   }
+
+   private cargarEquipo() {
+
+    this.http.get('https://angular-html-5e48e.firebaseio.com/equipo.json').subscribe( (resp: any[]) => {
+
+      this.equipo = resp;
+
+       } );
+   }
+}
